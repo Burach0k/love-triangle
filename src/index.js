37a86775cbb24be0a360cad651017d8058d683ef
Burach0k <Burak_andre@mail.ru>
2018-09-18 18:13:35 +0300

    yra

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    var j = 0;
+    var i = 0;
+    while( i<preferences.length){
+      var a = preferences[i];
+      var e = i;
+      var r = j;
+      var b = preferences[a -1];
+      if(a != b) {
+        var c = preferences[b -1];
+        if(b!= c) {
+          var d = preferences[c -1];
+          var q = c-1;
+            if((a == d)&&(i == q)) {
+                j++;
+            }
+          }
+        }
+      i = i + 1;
+    }
+   return j/3;
+  }
